d04b58809c4991258822fa1ce345cb50562f8b2b
小啊飞 <1772293339@qq.com>
2023-03-08 15:15:36 +0800

    1

--- HMSX.Second.Plugin/HMSX.Second.Plugin/study/Class1.cs
+++ HMSX.Second.Plugin/HMSX.Second.Plugin/study/Class1.cs
@@ -5,7 +5,7 @@ using Kingdee.BOS.WebApi.ServicesStub;
 namespace Jac.XkDemo.BOS.WebApi
 {
     /// <summary>
-    /// 【WebApi】自定义WebApi接口
+    /// 【WebApi】自定义WebApi接口123
     /// </summary>
     public class CustomWebApiService : AbstractWebApiBusinessService
     {
