bef82779be282859bed1b77f88f5548e33e21288
泽祥 <1412786453@qq.com>
2023-03-08 17:55:24 +0800

    1

--- HMSX.Second.Plugin/HMSX.Second.Plugin/study/Class1.cs
+++ HMSX.Second.Plugin/HMSX.Second.Plugin/study/Class1.cs
@@ -5,7 +5,7 @@ using Kingdee.BOS.WebApi.ServicesStub;
 namespace Jac.XkDemo.BOS.WebApi
 {
     /// <summary>
-    /// 【WebApi】自定义WebApi接口123 4589110
+    /// 【WebApi】自定义WebApi接口123 458911
     /// </summary>
     public class CustomWebApiService : AbstractWebApiBusinessService
     {
